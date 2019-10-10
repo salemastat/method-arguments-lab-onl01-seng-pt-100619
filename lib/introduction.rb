cf5dede49896ee17e2f43cd5001fd2701d1f3adf
Salem Asrat <salemasrat@gmail.com>
2019-10-10 00:55:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name}. I'm learning #{language}."
+  puts "Hi, my name is #{name}. I'm learning a #{language}."
 end
 
 introduction("Josh", "Ruby")
